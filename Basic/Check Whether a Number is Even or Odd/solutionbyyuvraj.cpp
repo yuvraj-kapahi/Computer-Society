68e638381abf9f47d8d58605168de4f77296bcb3
yuvraj-kapahi <55944426+yuvraj-kapahi@users.noreply.github.com>
2020-04-17 20:07:58 +0530

    Create solutionbyyuvraj.cpp

---
+++ Basic/Check Whether a Number is Even or Odd/solutionbyyuvraj.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int m;
+    cout<<"Enter the no:";
+    cin>>m;
+    if(m%2==0)
+    {cout<<"Even";
+    }
+    else
+    {cout<<"Odd";
+    }
+    
+    return 0;
+}
