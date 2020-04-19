92d7e0029850cbd5f8088cc2177c48a75d200d0f
yuvraj-kapahi <55944426+yuvraj-kapahi@users.noreply.github.com>
2020-04-19 14:31:57 +0530

    Create solutionbyyuvraj.cpp

---
+++ Basic/Calculate Factorial of A Number/solutionbyyuvraj.cpp
@@ -0,0 +1,14 @@
+int main()
+{
+    int n,j,i;
+    cout<<"Enter the no";
+    cin>>n;
+    cout<<"find factorial upto:";
+    cin>>j;
+    for(i=1;i<=j;i++)
+    {n=n*i;
+    }
+    cout<<n;
+
+    return 0;
+}
